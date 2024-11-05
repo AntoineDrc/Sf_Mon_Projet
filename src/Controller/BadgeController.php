60edf891b899abee13c510219d58096c5b04680a
Antoine Delubriac <Antoine.DELUBRIAC@stagiaire-elan.fr>
2024-11-05 14:30:38 +0100

    infos User header

---
+++ src/Controller/BadgeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class BadgeController extends AbstractController
+{
+    #[Route('/badge', name: 'badge')]
+    public function index(): Response
+    {
+        return $this->render('badge/index.html.twig', [
+            'controller_name' => 'BadgeController',
+        ]);
+    }
+}
--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -6,13 +6,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+
 class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'dashboard')]
     public function index(): Response
     {
+
+        // Récupère l'utilisateur connecté
+        /** @var User $user */
+        $user = $this->getUser();
+
+
         return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
+            'user' => $user,
         ]);
     }
 }
